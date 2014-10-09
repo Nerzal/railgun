154b9e7b270caf66be027006ec2e37ea8328e5c6
lfuelling <lukas@k40s.net>
2014-10-09 12:08:47 +0200

    Added Initial Project

---
+++ android/src/net/k40s/railgun/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package net.k40s.railgun.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import net.k40s.railgun.RailgunGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new RailgunGame(), config);
+	}
+}
