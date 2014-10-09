295fbe09427828b647171edd7457896e8bf952af
lfuelling <lukas@k40s.net>
2014-10-10 01:08:41 +0200

    Initial Commit

--- README.md
+++
@@ -1,4 +0,0 @@
-railgun [![Build Status](http://ci.k40s.net/buildStatus/icon?job=railgun)](http://ci.k40s.net/job/railgun/)
-=======
-
-Rail Shooter for Android. Made using libGDX.
--- android/src/net/k40s/railgun/android/AndroidLauncher.java
+++ android/src/net/k40s/railgun/android/AndroidLauncher.java
@@ -1,16 +1,31 @@
 package net.k40s.railgun.android;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import net.k40s.railgun.RailgunGame;
+import net.k40s.railgun.RailgunMain;
 
-public class AndroidLauncher extends AndroidApplication {
+import com.google.android.gms.games.leaderboard.LeaderboardBuffer;
+import com.google.android.gms.games.leaderboard.LeaderboardScoreBuffer;;
+import com.google.example.games.basegameutils.GameHelper;
+import com.google.example.games.basegameutils.GameHelper.GameHelperListener;
+
+public class AndroidLauncher extends AndroidApplication /* implements GameHelperListener, com.google.example.games.basegameutils.GoogleInterface */ {
+
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new RailgunGame(), config);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		initialize(new RailgunMain(), config);
 	}
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    }
+
 }
