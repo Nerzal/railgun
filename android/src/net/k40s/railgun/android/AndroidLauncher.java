2de49f49b88150de0624a8a846954739947a55ce
lfuelling <lukas@k40s.net>
2014-10-10 11:50:28 +0200

    Finished google implementation

--- android/src/net/k40s/railgun/android/AndroidLauncher.java
+++ android/src/net/k40s/railgun/android/AndroidLauncher.java
@@ -4,13 +4,75 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.google.example.games.basegameutils.GameHelper;
+
 import net.k40s.railgun.RailgunMain;
 
-public class AndroidLauncher extends AndroidApplication {
+public class AndroidLauncher extends AndroidApplication implements GameHelper.GameHelperListener, com.google.example.games.basegameutils.GoogleInterface{
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new RailgunMain(), config);
 	}
+
+    @Override
+    public void onSignInFailed() {
+
+    }
+
+    @Override
+    public void onSignInSucceeded() {
+
+    }
+
+    @Override
+    public void login() {
+        
+    }
+
+    @Override
+    public void logout() {
+
+    }
+
+    @Override
+    public boolean getSignedIn() {
+        return false;
+    }
+
+    @Override
+    public void submitScore(int score) {
+
+    }
+
+    @Override
+    public void submitAchievement(String id) {
+
+    }
+
+    @Override
+    public void incrementAchievement(String id, int steps) {
+
+    }
+
+    @Override
+    public void showScores() {
+
+    }
+
+    @Override
+    public void getScoresData() {
+
+    }
+
+    @Override
+    public boolean isConnected() {
+        return false;
+    }
+
+    @Override
+    public void showAchievements() {
+
+    }
 }
--- core/src/net/k40s/railgun/RailgunMain.java
+++ core/src/net/k40s/railgun/RailgunMain.java
@@ -1,12 +1,13 @@
 package net.k40s.railgun;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class RailgunMain extends ApplicationAdapter {
+public class RailgunMain extends Game {
 	SpriteBatch batch;
 	Texture img;
 	
