0c23d800f88640d5da92c4fb472ee3502d25bfd8
Nerzal <theel.tobias@gmx.de>
2014-10-11 14:08:06 +0200

    GameScreen:  - Added:    - InputProcessor    - camera.unproject() to the .render() Method

--- core/src/net/k40s/railgun/GameScreen.java
+++ core/src/net/k40s/railgun/GameScreen.java
@@ -1,15 +1,17 @@
 package net.k40s.railgun;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector3;
 
 /**
  * Created by k40s on 10/10/14.
  */
-public class GameScreen implements Screen {
+public class GameScreen implements Screen, InputProcessor {
 
     final RailgunMain game;
     OrthographicCamera camera;
@@ -19,9 +21,11 @@ public class GameScreen implements Screen {
     int lastPosY;
     int halfWidth;
     int halfHeight;
+	private Vector3 touchPoint;
 
     public GameScreen(final RailgunMain game) {
         this.game = game;
+        touchPoint = new Vector3();
         camera = new OrthographicCamera();
         camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         stateTime = 0f;
@@ -43,12 +47,14 @@ public class GameScreen implements Screen {
         game.batch.draw(Assets.gameBackground, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         stateTime += Gdx.graphics.getDeltaTime();
         currentFrame = Assets.spa.getKeyFrame(stateTime, true);
+		
         if(Gdx.input.isTouched()){
-            lastPosX = Gdx.input.getX()-32;
-            lastPosY = Gdx.input.getY()-32;
-            game.batch.draw(currentFrame, lastPosX, lastPosY, 128, 128);
+			touchPoint.x = Gdx.input.getX()-32;
+			touchPoint.y = Gdx.input.getY()-32;
+            camera.unproject(touchPoint); // Hier werden die screenKoordinaten mit den openGL koordinaten auf einen Nenner gebracht
+            game.batch.draw(currentFrame, touchPoint.x, touchPoint.y, 128, 128);
         } else {
-            game.batch.draw(currentFrame, lastPosX, lastPosY, 128, 128);
+            game.batch.draw(currentFrame, touchPoint.x, touchPoint.y, 128, 128);
 
             if(lastPosX < halfWidth-32){
                 lastPosX++;
@@ -95,4 +101,52 @@ public class GameScreen implements Screen {
     public void dispose() {
 
     }
+
+	@Override
+	public boolean keyDown(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
